1189733efa4aafeba7f434db0a99627c54ac268e
mikhael-pakulski <mikhaelpj2@gmail.com>
2025-05-18 22:46:31 -0300

    Create Serial.h

---
+++ Serial.h
@@ -0,0 +1,25 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+// Variável global representando o estado do caixa
+extern volatile char estado_caixa;
+
+void USART_Init(void);
+char USART_Transmit(unsigned char dado);
+
+void caixa_liberado();
+void caixa_travado();
+void caixa_data_hora();
+void caixa_entrada_cliente(char* usuario, char* senha);
+void caixa_saldo();
+void caixa_saque(char* valor_saque);
+void caixa_boleto_recebido();
+void caixa_pagamento(char* banco_convenio_valor);
+void imprime_comprovante(); // A definir no futuro
+void sessao_finalizada();
+void caixa_operando_normalmente();
+
+// Implementação opção A
+void caixa_troca_senha(char* usuario);
+
+#endif
