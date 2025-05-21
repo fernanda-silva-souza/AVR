48eecbe9801dc56ff45827e30aec082ca65c3537
mikhael-pakulski <mikhaelpj2@gmail.com>
2025-05-20 22:16:44 -0300

    Update Serial.h

--- Serial.h
+++ Serial.h
@@ -1,26 +1,29 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-// Variável global representando o estado do caixa
-volatile char estado_caixa;
+#include <stdint.h>
+
+// Variável global que indica o estado do caixa
 extern volatile char estado_caixa;
 
+// Inicialização da USART
 void USART_Init(void);
-char USART_Transmit(unsigned char dado);
+char USART_Transmit(uint8_t dado);
 
-void caixa_liberado();
-void caixa_travado();
-void caixa_data_hora();
+// Comunicação com o servidor
+void caixa_liberado(void);
+void caixa_travado(void);
+void caixa_data_hora(void);
 void caixa_entrada_cliente(char* usuario, char* senha);
-void caixa_saldo();
+void caixa_saldo(void);
 void caixa_saque(char* valor_saque);
-void caixa_boleto_recebido();
 void caixa_pagamento(char* banco_convenio_valor);
-void imprime_comprovante(); // A definir no futuro
-void sessao_finalizada();
-void caixa_operando_normalmente();
+void caixa_boleto_recebido(void);
+void imprime_comprovante(void);
+void sessao_finalizada(void);
+void caixa_operando_normalmente(void);
 
-// Implementação opção A
+// OPÇÃO A — Senha silábica
 void caixa_troca_senha(char* usuario);
 
-#endif
+#endif /* SERIAL_H */
