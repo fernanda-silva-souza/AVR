82dec08153bc5a76ebd2be209337287945f80c06
mikhael-pakulski <mikhaelpj2@gmail.com>
2025-05-20 22:18:52 -0300

    Create Funcoes.c

---
+++ Funcoes.c
@@ -0,0 +1,19 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "Timer.h"
+#include "Funcoes.h"
+
+extern volatile char inatividade_segundos;
+extern volatile char piscar_led;
+
+// Esta função deve ser chamada sempre que houver interação do usuário.
+// Ela zera o contador de inatividade e para o piscar do LED.
+void reset_inatividade() {
+	inatividade_segundos = 0;
+	piscar_led = 0;
+	timer3_stop();
+
+	// Caso tenha LED físico, pode desligá-lo aqui:
+	// PORTB &= ~(1 << PB7);
+}
