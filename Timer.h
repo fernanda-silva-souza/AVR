73cde3dbf8d9dc981615faf0b1ac73b6d56def16
mikhael-pakulski <mikhaelpj2@gmail.com>
2025-05-23 15:14:29 -0300

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -4,5 +4,9 @@
 void timer1_ctc_init(void);  // Inicia temporizador de inatividade
 void timer3_ctc_init(void);  // Inicia temporizador para piscar LED
 void timer3_stop(void);      // Para o timer que pisca LED
+void delay_1ms(void);		//Delay 1ms
+void delay_2ms(void);		//Delay 2ms
+void delay_10ms(void);		//Delay 10ms
+
 
 #endif /* TIMER_H_ */
