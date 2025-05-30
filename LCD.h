2ff67d27c28945200f679176b4e4bcdb9a6c94b6
mikhael-pakulski <mikhaelpj2@gmail.com>
2025-05-30 00:42:35 -0300

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -1,13 +1,13 @@
 #ifndef LCD_H_
 #define LCD_H_
 
-void configura_pinos_lcd();
+void configura_pinos_lcd(void);
 void envia_dados_lcd(char dado);
-void pulso_enable();
+void pulso_enable(void);
 void lcd_comando(char comando);
 void lcd_dado(char dado);
-void inicializa_lcd();
-void lcd_limpar();
+void inicializa_lcd(void);
+void lcd_limpar(void);
 void lcd_string(char texto[]);
 
 #endif /* LCD_H_ */
