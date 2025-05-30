dc05e9c8acf54bc636735df746f98b9abc268ce6
mikhael-pakulski <mikhaelpj2@gmail.com>
2025-05-30 00:43:42 -0300

    Update Timer.c

--- Timer.c
+++ Timer.c
@@ -4,26 +4,25 @@
 #include <util/delay.h>
 #include "Timer.h"
 
-// Timer1 — controla o tempo de inatividade (1s por interrupção)
+// Inicializa Timer1 no modo CTC para gerar interrupções a cada 1 segundo
 void timer1_ctc_init(void) {
-	TCCR1B = 0x0C;     // CTC mode, prescaler 256
+	TCCR1B = 0x0C;           // Modo CTC, prescaler 256
 	TCCR1A = 0;
-	OCR1A = 62499;     // 1 segundo (16MHz / 256 / (62499 + 1) = 1 Hz)
+	OCR1A = 62499;           // 1 Hz (1s)
 	TIMSK1 |= (1 << OCIE1A); // Habilita interrupção no compare
 }
 
-// Timer3 — pisca o LED a 2 Hz nos últimos 12s
+// Inicializa Timer3 no modo CTC para piscar LED a 2 Hz
 void timer3_ctc_init(void) {
-	TCCR3B = 0x0C;     // CTC mode, prescaler 256
+	TCCR3B = 0x0C;           // Modo CTC, prescaler 256
 	TCCR3A = 0;
-	OCR3A = 15624;     // 2 Hz (16MHz / 256 / (31249 + 1) = 2 Hz)
+	OCR3A = 15624;           // 2 Hz
 	TIMSK3 |= (1 << OCIE3A); // Habilita interrupção no compare
 }
 
-// Para o timer3 (piscar LED)
+// Para o Timer3 e desliga o LED
 void timer3_stop(void) {
 	TCCR3B = 0;
 	TIMSK3 &= ~(1 << OCIE3A);
-	PORTA &= ~(1 << PA0);  // Desliga LED ao encerrar sessão
+	PORTA &= ~(1 << PA0); // Desliga LED
 }
-
