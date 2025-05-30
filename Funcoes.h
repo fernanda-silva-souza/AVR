bc69e84b7fc2cf5347013b5b06644b99b2dc527f
mikhael-pakulski <mikhaelpj2@gmail.com>
2025-05-30 00:41:42 -0300

    Update Funcoes.h

--- Funcoes.h
+++ Funcoes.h
@@ -1,7 +1,13 @@
 #ifndef FUNCOES_H_
 #define FUNCOES_H_
 
-// Reseta o contador de inatividade e o LED
+void tela_bem_vindo(void);
+void tela_senha(void);
+void login_completo(void);
+void senha_invalida(void);
+void acesso_negado(void);
+void oferecer_impressao_comprovante(void);
+void menu_operacoes(void);
 void reset_inatividade(void);
 
 #endif /* FUNCOES_H_ */
